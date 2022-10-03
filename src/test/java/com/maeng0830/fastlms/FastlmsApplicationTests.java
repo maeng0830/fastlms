47d989a030d4a3fe0908a7b0985b4281853962d5
maeng0830 <kmk930830@gmail.com>
2022-10-03 10:33:25 +0900

    fastlms 초기화 설정

---
+++ src/test/java/com/maeng0830/fastlms/FastlmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maeng0830.fastlms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastlmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
